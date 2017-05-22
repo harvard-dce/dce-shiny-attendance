d7672961dd63235a735f34ba71d218e252bb1167
Jay Luker <jay_luker@harvard.edu>
2017-05-22 15:59:06 -0400

    fiddling with reactive events

--- es.R
+++ es.R
@@ -3,22 +3,41 @@ make_query <- function(q) {
   elasticsearchr::query(q)
 }
 
-for_everything <- make_query('{ "match_all": {} }')
+for_everything <- function() {
+  make_query('{ "match_all": {} }')
+}
 
-episodes_by_term <- function(year_term) {
+by_term <- function(year_term) {
   year_term <- str_split(year_term, "-")
   year <- year_term[[1]][1]
   term <- year_term[[1]][2]
-  by_term <- make_query(
+  make_query(
     str_interp('{
       "bool": {
         "must": [
              { "term": { "year": "${year}" }},
-             { "term": { "term": "${term}" }}        
+             { "term": { "term": "${term}" }}
         ]
-      }    
+      }
     }')
   )
-  elastic(es_host, "episodes") %search% by_term
+}
+
+by_series <- function(series) {
+  make_query(
+    str_interp('{
+      "term": {
+        "series": "${series}"
+      }
+    }')
+  )
+}
+
+episodes_by_term <- function(year_term) {
+  elastic(es_host, "episodes") %search% by_term(year_term)
+}
+
+episodes_by_series <- function(series, year_term) {
+  elastic(es_host, "episodes") %search% (by_term(year_term) + by_series(series))
 }
 
--- server.R
+++ server.R
@@ -7,26 +7,47 @@
 
 shinyServer(function(input, output, session) {
 
+  episodes <- episodes_by_term(default_term)
+
+  generateCourseChoices <- function(term) {
+    courses <- dplyr::distinct(episodes, series, course)
+    courses <- courses[order(courses$course),]
+    with(courses, split(series, course))
+  }
+
   cdata <- session$clientData
 
-  episodes <- reactiveValues(episodes=episodes_by_term(default_term))
+  # episodes <- reactiveValues(episodes=episodes_by_term(default_term))
+  #
+  # observeEvent(input$term, {
+  #   episodes$episodes <- episodes_by_term(input$term)
+  # }, ignoreInit = T)
+  #
+  # observe({
+  #   output$courseSelect <- renderUI({
+  #     courses <- dplyr::distinct(episodes$episodes, series, course)
+  #     courses <- courses[order(courses$course),]
+  #     courseChoices <- with(courses, split(series, course))
+  #     selectInput("course", "Course:", courseChoices)
+  #   })
+  # })
 
-  observeEvent(input$term, {
-    episodes$episodes <- episodes_by_term(input$term)
-  }, ignoreInit = T)
 
-  observe({
-    output$courseSelect <- renderUI({
-      courses <- dplyr::distinct(episodes$episodes, series, course)
-      courses <- courses[order(courses$course),]
-      courseChoices <- with(courses, split(series, course))
-      selectInput("course", "Course:", courseChoices)
-    })
+  output$courseSelect <- renderUI({
+    courseChoices <- generateCourseChoices(default_term)
+    selectInput("course", "Course:", courseChoices)
   })
 
   observe({
+    courseChoices <- generateCourseChoices(input$term)
+    isolate({
+      updateSelectInput(session, "course", choices=courseChoices)
+    })
+  })
+
+  observeEvent(input$course, {
     output$lectureTable <- renderDataTable({
-      lectures <- dplyr::filter(episodes$episodes, series == input$course)
+      lectures <- episodes_by_series(input$course, input$term)
       lectures[order(lectures$title),]
     })
   })
