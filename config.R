463551297065dcbd4aa65817f3a9193318b655bb
Jay Luker <jay_luker@harvard.edu>
2017-05-30 16:54:16 -0400

    use a `.env` file for some config settings

--- config.R
+++ config.R
@@ -1,7 +1,18 @@
-es.host <- 'http://localhost:9200'
-es.episode.index <- 'episodes.2017.05.26'
-default.term <- "2017-02"
+library(dotenv)
+
+env <- function(key, default=NA) {
+  val <- Sys.getenv(key)
+  if (val == "" && !is.na(default)) {
+    val <- default
+  }
+  val
+}
+
+es.host <- env("ES_HOST", 'http://localhost:9200')
+es.episode.index <- env("ES_EPISODE_INDEX", 'episodes')
+default.term <- env("DEFAULT_TERM", "2017-02")
 default.tz <- "America/New_York"
+
 term.options <- c(
   "Spring 2017" = "2017-02",
   "Fall 2016" = "2017-01"
